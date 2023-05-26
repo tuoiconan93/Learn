48ebeb0586e11e333dc886e812d2a1d81ace6000
tuoiconan93 <tuoi.letan@gmail.com>
2023-05-26 16:24:30 +0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { OrderComponent } from './order/order.component';
+import { ServiceComponent } from './service/service.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    PageNotFoundComponent,
+    OrderComponent,
+    ServiceComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public Discs: any[]=[];
+  public cities: any[]=[];
+  public ward: any[]=[];
 
+  constructor(private http: HttpClient) { }
+  public ngOnInit(): void {
+    this.http.get('assets/data.json').subscribe((data:any) => {
+      this.cities=data;
+    });
+  }
+  public changeCity(event: any) {
+    const name = event.target.value
+   
+    this.Discs=this.cities.find(data => data.Name===name)?.Districts || []
+
+  }
+  public changeDisc(event: any) {
+    const name = event.target.value
+   
+    this.ward=this.Discs.find(data => data.Name===name)?.Wards || []
+  }
+  public changeWards(event: any) {
+    const name = event.target.value
+   
+  }
+  
 }
+
