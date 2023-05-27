458d26ae9bd8fed2b26db35759afefd207fb7a47
tuoiconan93 <tuoi.letan@gmail.com>
2023-05-27 13:22:06 +0700

    1

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import{ faBackwardFast,faBackwardStep,faForwardFast,faForwardStep } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBackwardFast,
+  faBackwardStep,
+  faForwardFast,
+  faForwardStep,
+} from '@fortawesome/free-solid-svg-icons';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
 })
-
 export class OrderComponent implements OnInit {
-  pagesize=100;
-  faBackwardFast=faBackwardFast;faBackwardStep=faBackwardStep;faForwardFast=faForwardFast;faForwardStep=faForwardStep;
- 
+  constructor(private http: HttpClient) {}
+  public search: string='';
+  public pagesize: number = 100;
+  public orderlists:any[] = [];
+  public pages: number[]=[];
+  pageCount: number=0;
+  faBackwardFast = faBackwardFast;
+  faBackwardStep = faBackwardStep;
+  faForwardFast = faForwardFast;
+  faForwardStep = faForwardStep;
   public ngOnInit(): void {
+    this.http.get('/assets/db/orderlist.json').subscribe((data: any) => {
+      this.orderlists = data;
+      this.pageCount=Math.ceil(this.orderlists.length/this.pagesize);
+      console.log(this.pageCount);
+
+    });
+    
   }
 
 }
