d242efa2c2db3e6d800ce1f04a53bf0a8aa62da6
tuoiconan93 <tuoi.letan@gmail.com>
2023-05-28 16:02:00 +0700

    1

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -13,75 +13,92 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./order.component.css'],
 })
 export class OrderComponent implements OnInit {
- 
   public search: string = '';
   public pagesize: number = 100;
   public orderlists: any[] = [];
-  public pages: number[] = [];
-  public currentPage: number=1;
-  pageCount: number = 0;
+
+  public currentPage: number = 1;
+  public pageCount: number = 0;
+  public startitemshow: number=0;
   faBackwardFast = faBackwardFast;
   faBackwardStep = faBackwardStep;
   faForwardFast = faForwardFast;
   faForwardStep = faForwardStep;
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
+  //get data and tinh so luong trang
   public ngOnInit(): void {
     this.http.get('/assets/db/orderlist.json').subscribe((data: any) => {
       this.orderlists = data;
       this.pageCount = Math.ceil(this.orderlists.length / this.pagesize);
+      this.updateStartItemShow();
+      console.log(this.pageCount);
     });
   }
+  //tinh trang hien tai
   public getPages(): number[] {
     const pages: number[] = [];
     const startPage = Math.floor((this.currentPage - 1) / 10) * 10 + 1;
     let endPage = startPage + 9;
-  
+
     if (endPage > this.pageCount) {
       endPage = this.pageCount;
     }
-  
+
     for (let i = startPage; i <= endPage; i++) {
       pages.push(i);
     }
-  
+    this.updateStartItemShow();
     return pages;
   }
+  //trang hien tai bang page show
   public setcurrentPage(page: number): void {
     this.currentPage = page;
+    this.updateStartItemShow();
   }
-  previous10Page(): number{
-    if(this.currentPage>10){
-this.currentPage-=10;
-    }else{
-      this.currentPage=1;
+  previous10Page(): void {
+    if (this.currentPage > 10) {
+      this.currentPage -= 10;
+    } else {
+      this.currentPage = 1;
     }
-    return this.currentPage;
-
+    this.updateStartItemShow();  
   }
-  previousPage(): number{
-    if(this.currentPage>1){
+  previousPage(): void {
+    if (this.currentPage > 1) {
       this.currentPage--;
-    }else{
-      this.currentPage=1;
+    } else {
+      this.currentPage = 1;
     }
-    return this.currentPage;
+    this.updateStartItemShow(); 
   }
-  nextPage(): number{
-    if(this.currentPage<this.pageCount){
+  //
+  nextPage(): void {
+    if (this.currentPage < this.pageCount) {
       this.currentPage++;
-    }else{
-      this.currentPage=this.pageCount;
+    } else {
+      this.currentPage = this.pageCount;
     }
-return this.currentPage;
+    this.updateStartItemShow();   
   }
-  next10Page(): number{
-    if(this.currentPage<this.pageCount-10){
-this.currentPage+=10;
-    }else{
-      this.currentPage=this.pageCount;
+  //next 10 trang
+  next10Page(): void {
+    if (this.currentPage < this.pageCount - 10) {
+      this.currentPage += 10;
+    } else {
+      this.currentPage = this.pageCount;
     }
-    return this.currentPage;
-
+    this.updateStartItemShow();
   }
+  //hien thi trang theo currenpage
+  public updateStartItemShow(): void {
+    this.startitemshow = (this.currentPage - 1) * this.pagesize;
+  }
+  //thay doi so luong hien thi
+  public repaginate(): void {
+    this.startitemshow = 0;
+    this.currentPage=1;
+    this.updateStartItemShow();
+    this.pageCount = Math.ceil(this.orderlists.length / this.pagesize);
+  }
+  
 }
