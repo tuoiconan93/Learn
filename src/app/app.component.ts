3d58a2f1e7f06b2965d4982292b23f198eb6597f
tuoiconan93 <tuoi.letan@gmail.com>
2023-06-16 11:23:07 +0700

    reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Order';
+export class AppComponent implements OnInit{
+  private timeout: any;
+  //reload page
+  public ngOnInit(): void {
+    this.resetTimeout();
+    window.addEventListener('click', this.resetTimeout.bind(this));
+    window.addEventListener('mousemove', this.resetTimeout.bind(this));
+    window.addEventListener('keypress', this.resetTimeout.bind(this));
+  }
+    private resetTimeout(): void {
+      if (this.timeout) {
+        clearTimeout(this.timeout);
+      }
+    
+      this.timeout = setTimeout(() => {
+        window.location.reload();
+      }, 30 * 60 * 1000); // 30 phút = 30 * 60 * 1000 milliseconds
+    }
+    
+  public ngOnDestroy(): void {
+    window.removeEventListener('click', this.resetTimeout);
+    window.removeEventListener('mousemove', this.resetTimeout);
+    window.removeEventListener('keypress', this.resetTimeout);
+  }
+  
 }
